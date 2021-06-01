cf851251fbc82029047dbe409360d158155c7f17
faithdinno <adebayooluwadara@gmail.com>
2021-06-01 14:05:55 +0100

    latest changes

---
+++ database/migrations/2021_06_01_125225_create_verifications_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVerificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('verifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->nullable();
+            $table->string('type')->nullable();
+            $table->string('image1')->nullable();
+            $table->string('image2')->nullable();
+            $table->string('number')->nullable();
+            $table->string('date')->nullable();
+            $table->integer('status')->nullable();
+            $table->text('selfie')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('verifications');
+    }
+}
